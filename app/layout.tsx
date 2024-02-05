180b8aceeaf6a64dee371d988923cab5706ec3cb
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2024-02-05 11:55:25 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         <nav>
           <a href="#welcome_section">main</a>
